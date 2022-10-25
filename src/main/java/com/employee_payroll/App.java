87efedb3dc458af2e154265bb37406b9018f3e31
Rajshriwagh <mahajan.rajshri16@gmail.com>
2022-10-25 23:03:13 +0530

    Initial commit

---
+++ src/main/java/com/employee_payroll/App.java
@@ -0,0 +1,10 @@
+package com.employee_payroll;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Payroll Service Problem !" );
+    }
+}
