a0c778e147c49b51e74fbed44f8062f1540633cf
Rajshriwagh <mahajan.rajshri16@gmail.com>
2022-10-26 23:54:12 +0530

    Add: Watch service to watch particular directory

--- src/main/java/com/employee_payroll/FileUtils.java
+++ src/main/java/com/employee_payroll/FileUtils.java
@@ -1,5 +1,14 @@
 package com.employee_payroll;
+import java.io.File;
 
 public class FileUtils {
-
+	public static boolean deleteFiles(File contentsToDelete){
+        File[] allCounts = contentsToDelete.listFiles();
+        if (allCounts != null){
+            for (File file : allCounts) {
+                deleteFiles(file);
+            }
+        }
+        return contentsToDelete.delete();
+    }
 }
